153be521d46f4a7f44c9581e1818dec64d6e9b3a
auppunda <auppunda@gmail.com>
2018-12-03 14:33:34 -0800

    added person class, added current user to map
    Signed-off-by: auppunda <auppunda@gmail.com>

--- src/person.js
+++ src/person.js
@@ -1,37 +1,9 @@
-// import React from 'react';
+export class Person { 
+	
+	constructor(id, email, username) {
+    	this.id = id
+    	this.email = email
+    	this.username = username
+  	}
 
-// export class Person extends React.Component { 
-// 	constructor() {
-//     	super();
-//     	this.state = {
-//         	myLatLng: {
-//             	lat: 49.2827,
-//             	lng: -123.1207
-//         	}
-//     	};
-//   	}
-//   	getLocation() {
-//         if (navigator.geolocation) {
-//             navigator.geolocation.getCurrentPosition((position) => {
-//                 this.setState({
-//                         myLatLng: {
-//                             lat: position.coords.latitude,
-//                             lng: position.coords.longitude
-//                         }
-//                     }
-//                 );
-//             })
-//         } else {
-//             //browser doesn't support geolocation, set as vancouver
-//             this.setState({
-//                     myLatLng: {
-//                         lat: 49.8527,
-//                         lng: -123.1207
-//                     }
-//                 }
-//             );
-//         }
-//     }
-
-
-// }
+}
