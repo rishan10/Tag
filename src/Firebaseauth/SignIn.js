73ff8f6de27d19d7bd814ae8469c12277411b094
aaronphilip <aaronpphilip@gmail.com>
2018-12-03 21:55:28 -0800

    fix login bug

--- src/Firebaseauth/SignIn.js
+++ src/Firebaseauth/SignIn.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
 import './css/style.css';
+import $ from 'jquery';
 
 class SignIn extends Component {
+
+  componentDidMount() {
+    $('.message a').click(function(){
+       $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    });
+  }
 
   render() {
     return (
       <div>
-        <div class="login-page">
-        <div class="form">
-          <form class="register-form">
+        <div className="login-page">
+        <div className="form">
+          <form className="register-form">
             <input id= "userName" type="text" placeholder="name"/>
             <input id= "userPassword" type="password" placeholder="password"/>
             <input id="userEmail" type="text" placeholder="email address"/>
             <button id= "signup">create</button>
-            <p class="message">Already registered? <a href="#">Sign In</a></p>
+            <p className="message">Already registered? <a href="#">Sign In</a></p>
           </form>
-          <form class="login-form">
+          <form className="login-form">
             <input id="signInEmail" type="text" placeholder="email"/>
             <input id= "signInPassword" type="password" placeholder="password"/>
             <button id="signin">login</button>
-            <p class="message">Not registered? <a href="#">Create an account</a></p>
+            <p className="message">Not registered? <a href="#">Create an account</a></p>
           </form>
         </div>
       </div>
         <script src="https://www.gstatic.com/firebasejs/5.6.0/firebase.js"></script>
-        <script src="auth.js">
-        </script>
         <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-        
-
-          <script  src="Firebaseauth/js/index.js"></script>
       </div>
     );
   }
