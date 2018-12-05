0c9fa1d19e13fe2b8868bb733cf1bf481f97758b
Ryan Tong <ryanwrtong@gmail.com>
2018-12-04 20:20:21 -0800

    firebase location updates

--- public/userdata.js
+++ public/userdata.js
@@ -1,6 +1,6 @@
 //gives back database information about the users as JSON
-const preObject = document.getElementById('object');
-const dbRefObject = firebase.database().ref().child('users');
-dbRefObject.on('value', data => {
-    console.log(preObject.innerText = JSON.stringify(data.val())) //gives back user data in JSON format
-});
+    const preObject = document.getElementById('object');
+    const dbRefObject = firebase.database().ref().child('users');
+    dbRefObject.on('value', data => {
+        console.log(preObject.innerText = JSON.stringify(data.val())) //gives back user data in JSON format
+    });
